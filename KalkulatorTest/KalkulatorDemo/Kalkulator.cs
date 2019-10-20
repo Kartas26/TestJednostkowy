c594f7ca0f2740bcc1fa3bf7d6db31b39c36197f
Kartas26 <48808861+Kartas26@users.noreply.github.com>
2019-10-20 20:36:31 +0200

    Add project files.

---
+++ KalkulatorTest/KalkulatorDemo/Kalkulator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KalkulatorDemo
+{
+    internal class Kalkulator
+    {
+        internal int Dodaj(int a, int b)
+        {
+            return a + b;
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ KalkulatorTest/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Compatibility;
+using NUnit.Framework;
+
+namespace KalkulatorTest
+{
+    public class Test
+    {
+        [Test]
+        public void DodajDwieDodatnie()
+        {
+            var kalk = new KalkulatorDemo.Kalkulator();
+            int sum = kalk.Dodaj(2, 2);
+            Assert.AreEqual(4, sum);
+        }
+
+        [Test]
+        public void DodajPierwszaUjemna()
+        {
+            var kalk = new KalkulatorDemo.Kalkulator();
+            int sum = kalk.Dodaj((-1), 2);
+            Assert.AreEqual(1, sum);
+        }
+
+        [Test]
+        public void DodajDrugaUjemna()
+        {
+            var kalk = new KalkulatorDemo.Kalkulator();
+            int sum = kalk.Dodaj(3, (-1));
+            Assert.AreEqual(2, sum);
+        }
+
+        [Test]
+        public void DodajDwieUjemne()
+        {
+            var kalk = new KalkulatorDemo.Kalkulator();
+            int sum = kalk.Dodaj((-2), (-2));
+            Assert.AreEqual((-4), sum);
+        }
+    }
+}
